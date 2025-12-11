baa2dda7169326d2655266f1c0b1e43381a24ec5
mendy hershkop <hers.mm@gmail.com>
2025-12-11 10:48:26 +0200

    adding increasing by category

--- trade/priceChange.js
+++ trade/priceChange.js
@@ -1,12 +1,20 @@
 import { stockMarket } from "../data/data.js";
 
-export default function increaseSelect(stock){
+export function increaseSelect(stock){
     let item = stockMarket['stocks'].find((element) => element.name == stock)
-    console.log(item);
     item.previousPrices.push(item.currentPrice)
     item.currentPrice = item.currentPrice*1.05
-    console.log(item);
+   
+}
+
+export function increaseCategory(stock, category){
+    let items = stockMarket['stocks'].filter((item)=> item.category == category)
+    console.log(items);
     
+    items.forEach((item) => {if (item.name != stock){
+        item.previousPrices.push(item.currentPrice)
+        item.currentPrice = item.currentPrice * 1.01
+    }})
+    console.log(items);
     
 }
-increase("BrightFuture Academy")
